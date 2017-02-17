7131f520bbe1911edfb47b491630e58048833957
Alen Bakovic <alen.bakovic0@gmail.com>
2017-02-17 13:48:25 +0100

    List of selected files added.

--- src/app/file-chooser/app.file-list.ts
+++ src/app/file-chooser/app.file-list.ts
@@ -4,8 +4,13 @@ import { FileUploader} from 'ng2-file-upload';
 @Component({
     moduleId: module.id,
     selector: 'rg-file-list',
-    templateUrl: 'app.file-list.html'
+    templateUrl: 'app.file-list.html',
+    styleUrls:['app.file-list.css']
 })
 export class RGFileList {
+    progress: string = "Overall Uplaod Progress";
+    filesToUpload: string = "Files To Upload:";
+    tableHeader: string[] = ["File Name", "Size", "Status", "Action"];   
+
     uploader: FileUploader = new FileUploader({url: 'http://localhost:3001/upload'});
 }
