3e30b6292a677247d0dfe48c96b875428215bea7
Alen Bakovic <alen.bakovic0@gmail.com>
2017-02-17 14:55:55 +0100

    Sucess alert added.

--- src/app/file-chooser/app.file-list.ts
+++ src/app/file-chooser/app.file-list.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
-import { FileUploader} from 'ng2-file-upload';
+import { FileUploader } from 'ng2-file-upload';
 
 @Component({
     moduleId: module.id,
     selector: 'rg-file-list',
     templateUrl: 'app.file-list.html',
-    styleUrls:['app.file-list.css']
+    styleUrls: ['app.file-list.css']
 })
 export class RGFileList {
     progress: string = "Overall Uplaod Progress";
     filesToUpload: string = "Files To Upload:";
-    tableHeader: string[] = ["File Name", "Size", "Status", "Action"];   
+    tableHeader: string[] = ["File Name", "Size", "Status", "Action"];
 
-    uploader: FileUploader = new FileUploader({url: 'http://localhost:3001/upload'});
+    uploader: FileUploader;
+
+    constructor() {
+        this.uploader =  new FileUploader({ url: 'http://localhost:3001/upload' });
+        this.uploader.onCompleteAll = () => {
+            console.log("onCompleteAll");
+        }
+    }    
+
 }
