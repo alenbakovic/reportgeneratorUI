952ce79c2af971a501504bfcdf630ae874fd6567
Alen Bakovic <alen.bakovic0@gmail.com>
2017-02-17 15:22:41 +0100

    Download report fiels button.

--- src/app/file-chooser/app.file-list.ts
+++ src/app/file-chooser/app.file-list.ts
@@ -12,13 +12,29 @@ export class RGFileList {
     filesToUpload: string = "Files To Upload:";
     tableHeader: string[] = ["File Name", "Size", "Status", "Action"];
 
+    uploadSucess: boolean = false;
+
     uploader: FileUploader;
 
     constructor() {
         this.uploader =  new FileUploader({ url: 'http://localhost:3001/upload' });
         this.uploader.onCompleteAll = () => {
             console.log("onCompleteAll");
+            this.uploadSucess = true;
         }
-    }    
+    }
+
+    uploadAll(): void {
+        this.uploader.uploadAll();
+    }
+
+    removeAll(): void {
+        this.uploader.clearQueue();
+        this.uploadSucess = false;
+    }
+
+    startFileDownload(): void {
+        console.log("Downloading files");
+    }
 
 }
